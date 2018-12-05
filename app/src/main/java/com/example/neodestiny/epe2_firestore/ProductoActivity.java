ae17172cb1ae7b54ae8ea0c045c8767552e381a7
neodestiny2 <david.diaz.cisternas@gmail.com>
2018-12-05 20:05:41 -0300

    Declaración de variables en el activity de los productos.

--- app/src/main/java/com/example/neodestiny/epe2_firestore/ProductoActivity.java
+++ app/src/main/java/com/example/neodestiny/epe2_firestore/ProductoActivity.java
@@ -2,17 +2,41 @@ package com.example.neodestiny.epe2_firestore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
+
+import com.example.neodestiny.epe2_firestore.model.Cliente;
+import com.example.neodestiny.epe2_firestore.model.Producto;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductoActivity extends AppCompatActivity {
 
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    Producto productoSelected;
+
+    private List<Producto> listCliente = new ArrayList<Producto>();
+    ArrayAdapter<Producto> arrayAdapterCliente;
     EditText et_nombreMascota, et_tipoAnimal, et_dueno;
     RadioButton rb_macho, rb_hembra;
-    
+    ListView lv_lista2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_producto);
+        et_nombreMascota=findViewById(R.id.et_nombreMascota);
+        et_tipoAnimal=findViewById(R.id.et_tipoAnimal);
+        et_dueno=findViewById(R.id.et_dueno);
+        rb_macho=findViewById(R.id.rb_macho);
+        rb_hembra=findViewById(R.id.rb_hembra);
+        lv_lista2=findViewById(R.id.lv_lista2);
     }
+
+
 }
