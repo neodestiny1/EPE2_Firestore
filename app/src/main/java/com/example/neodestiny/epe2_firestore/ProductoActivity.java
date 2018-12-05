364daf96a8968a097a0466391e022a8e9889a9af
neodestiny2 <david.diaz.cisternas@gmail.com>
2018-12-05 19:57:43 -0300

    Declaracion de los campos del layout a su respectivo activity.

--- app/src/main/java/com/example/neodestiny/epe2_firestore/ProductoActivity.java
+++ app/src/main/java/com/example/neodestiny/epe2_firestore/ProductoActivity.java
@@ -2,9 +2,14 @@ package com.example.neodestiny.epe2_firestore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class ProductoActivity extends AppCompatActivity {
 
+    EditText et_nombreMascota, et_tipoAnimal, et_dueno;
+    RadioButton rb_macho, rb_hembra;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
