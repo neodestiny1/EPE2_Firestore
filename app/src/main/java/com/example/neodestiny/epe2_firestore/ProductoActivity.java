af750dd2be52fad51fb09ab5424d5ada6740f4f8
neodestiny2 <david.diaz.cisternas@gmail.com>
2018-12-05 10:05:36 -0300

    Implementacion actividad de producto

---
+++ app/src/main/java/com/example/neodestiny/epe2_firestore/ProductoActivity.java
@@ -0,0 +1,13 @@
+package com.example.neodestiny.epe2_firestore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProductoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_producto);
+    }
+}
