de48b2f566041edca31bd2f6f911cd946838b311
neodestiny2 <david.diaz.cisternas@gmail.com>
2018-12-07 00:52:20 -0300

    (Nicole) Implementacion de metodos para cambio de actividades

--- app/src/main/java/com/example/neodestiny/epe2_firestore/MenuIngresoActivity.java
+++ app/src/main/java/com/example/neodestiny/epe2_firestore/MenuIngresoActivity.java
@@ -1,13 +1,34 @@
 package com.example.neodestiny.epe2_firestore;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuIngresoActivity extends AppCompatActivity {
+
+    Button btn_cliente, btn_mascota;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_ingreso);
+        btn_cliente = findViewById(R.id.btn_cliente);
+        btn_mascota = findViewById(R.id.btn_mascota);
+
+    }
+
+    public void seleccionMenu(View view) {
+        switch (view.getId()) {
+            case R.id.btn_cliente:
+                startActivity(new Intent(MenuIngresoActivity.this, MainActivity.class));
+                break;
+            case R.id.btn_mascota:
+                startActivity(new Intent(MenuIngresoActivity.this, ProductoActivity.class));
+                break;
+            default:
+                break;
+        }
     }
 }
