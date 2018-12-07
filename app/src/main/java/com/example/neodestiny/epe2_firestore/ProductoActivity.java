ab5be41ed173047c79d8b46c06eeeae26efbce5c
neodestiny2 <david.diaz.cisternas@gmail.com>
2018-12-06 22:36:19 -0300

    (Leonardo) Creacion de validaciones de campos vacios.

--- app/src/main/java/com/example/neodestiny/epe2_firestore/ProductoActivity.java
+++ app/src/main/java/com/example/neodestiny/epe2_firestore/ProductoActivity.java
@@ -11,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.example.neodestiny.epe2_firestore.model.Cliente;
 import com.example.neodestiny.epe2_firestore.model.Producto;
@@ -35,32 +36,32 @@ public class ProductoActivity extends AppCompatActivity {
     EditText et_nombreMascota, et_tipoAnimal, et_dueno;
     RadioButton rb_macho, rb_hembra;
     ListView lv_lista2;
+    RadioGroup rb_grupo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_producto);
-        et_nombreMascota=findViewById(R.id.et_nombreMascota);
-        et_tipoAnimal=findViewById(R.id.et_tipoAnimal);
-        et_dueno=findViewById(R.id.et_dueno);
-        rb_macho=findViewById(R.id.rb_macho);
-        rb_hembra=findViewById(R.id.rb_hembra);
-        lv_lista2=findViewById(R.id.lv_lista2);
+        et_nombreMascota = findViewById(R.id.et_nombreMascota);
+        et_tipoAnimal = findViewById(R.id.et_tipoAnimal);
+        et_dueno = findViewById(R.id.et_dueno);
+        rb_macho = findViewById(R.id.rb_macho);
+        rb_hembra = findViewById(R.id.rb_hembra);
+        lv_lista2 = findViewById(R.id.lv_lista2);
+        rb_grupo=findViewById(R.id.rb_grupo);
         inicializarFirebase();
         listarDatos();
 
         lv_lista2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                productoSelected=(Producto) parent.getItemAtPosition(position);
+                productoSelected = (Producto) parent.getItemAtPosition(position);
                 et_nombreMascota.setText(productoSelected.getNombre());
                 et_tipoAnimal.setText(productoSelected.getTipoAnimal());
                 et_dueno.setText(productoSelected.getNombreDueno());
-                if(productoSelected.getSexo().equals("Macho"))
-                {
+                if (productoSelected.getSexo().equals("Macho")) {
                     rb_macho.setChecked(true);
-                }
-                else
-                {
+                } else {
                     rb_hembra.setChecked(true);
                 }
             }
@@ -78,8 +79,7 @@ public class ProductoActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 listProducto.clear();
-                for(DataSnapshot objSnaptshot: dataSnapshot.getChildren())
-                {
+                for (DataSnapshot objSnaptshot : dataSnapshot.getChildren()) {
                     Producto p = objSnaptshot.getValue(Producto.class);
                     listProducto.add(p);
 
@@ -107,18 +107,54 @@ public class ProductoActivity extends AppCompatActivity {
         String nombre = et_nombreMascota.getText().toString();
         String tipoAnimal = et_tipoAnimal.getText().toString();
         String dueño = et_dueno.getText().toString();
-        String sexo="";
-        if(rb_macho.isChecked())
-        {
-            sexo="Macho";
-        }
-        else
-        {
-            sexo="Hembra";
+        String sexo = "";
+        if (rb_macho.isChecked()) {
+            sexo = "Macho";
+        } else if (rb_hembra.isChecked()){
+            sexo = "Hembra";
         }
 
-        
+        switch (item.getItemId()) {
+            case R.id.ic_add:
+                validaciones();
+                break;
+            case R.id.ic_save:
+                break;
+            case R.id.ic_delete:
+                break;
+        }
+
         return true;
     }
+
+    private void validaciones() {
+
+
+        String nombre = et_nombreMascota.getText().toString();
+        String tipoAnimal = et_tipoAnimal.getText().toString();
+        String dueño = et_dueno.getText().toString();
+        String sexo = "";
+
+        if (rb_macho.isChecked()) {
+            sexo = "Macho";
+        } else if (rb_hembra.isChecked()) {
+            sexo = "Hembra";
+        }
+        if (nombre.equals("")) {
+            et_nombreMascota.setError("Requerido");
+            et_nombreMascota.requestFocus();
+        } else if (tipoAnimal.equals("")) {
+            et_tipoAnimal.setError("Requerido");
+            et_tipoAnimal.requestFocus();
+        } else if (dueño.equals("")) {
+            et_dueno.setError("Requerido");
+            et_dueno.requestFocus();
+        } else if (sexo.equals("")) {
+            rb_macho.setError("Requerido");
+            rb_hembra.setError("Requerido");
+            rb_grupo.requestFocus();
+        }
+    }
+
 
 }
